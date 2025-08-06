bd7fc8fdf7a03883c8999b8e8352d8e0d8dae32a
unknown <leandro@pontogovsistemas.com.br>
2025-08-06 11:32:54 -0300

    feat: Desenvolvido melhor estilização e ordem alfebetica aos contatos

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ $(function () {
 	$("#botao-adicionar").on("click", function () {
 		if ($("#entrada").val() !== "") {
 			id++;
-			var li = $("<li style='display: none' id='" + id + "'>" +  $("#entrada").val() + "<button onclick=\"excluir(" + id + ")\">  X </button>" + "</li>");
+			var li = $("<li style='display: none' id='" + id + "'>" + $("#entrada").val() + "<button onclick=\"excluir(" + id + ")\">X</button>" + "</li>");
 
 			$("#lista").append(li);
-			li.fadeIn("fast");
+			li.fadeIn("slow");
+
+			ordenarLista();
+
 		} else {
 			alert("Não é possivel adicionar um contato vazio");
 		}
@@ -18,9 +21,18 @@ $("#lista").on("click", "li", function () {
 	$(this).toggleClass("mudar");
 });
 
-function excluir(id) {
-	$("li#" + id).fadeOut("slow", function(){
-		$(this).remove();
+function ordenarLista(){
+	const itens = $("#lista li").get();
+
+	itens.sort(function(a, b){
+		const textoA = $(a).text().toLowerCase();
+		const textoB = $(b).text().toLowerCase();
+
+		return textoA.localeCompare(textoB);
+	});
+
+	$.each(itens, function(index, item){
+		$("#lista").append(item);
 	});
 }
 
@@ -28,19 +40,39 @@ $("#sumir").on("click", function () {
 	$("#conteudo2").slideToggle("slow", function () {
 
 	});
+
+	var botao = $(this);
+
+		if(botao.text() === 'Fechar'){
+			botao.text('Abrir');
+		}else{
+			botao.text('Fechar');
+		}
 });
 
 $("#botao-pesquisar").on("click", function () {
 	var valor = $("#pesquisa").val().toLowerCase();
+	var naoTem = false;
 	$("li")
 		.filter(function () {
-		var texto = $(this).text().toLowerCase();
+			var texto = $(this).text().toLowerCase();
 
-        if (texto.indexOf(valor) > -1) {
-            $(this).show("slow");
-        } else {
-            $(this).hide("slow");
-        }
+			if (texto.indexOf(valor) > -1) {
+				$(this).show("slow");
+				naoTem = true;
+			} else {
+				$(this).hide("slow");
+			}
 		});
+
+	if (!naoTem) {
+		alert("Voce nao possui nenhum contato com esse nome");
+	}
 });
 
+function excluir(id) {
+	$("li#" + id).fadeOut("slow", function () {
+		$(this).remove();
+		ordenarLista();
+	});
+}
