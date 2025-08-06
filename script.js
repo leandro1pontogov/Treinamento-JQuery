1e656c8f44483c0b8bd8a5dc0716ea44d376ba9c
unknown <leandro@pontogovsistemas.com.br>
2025-08-06 09:47:09 -0300

    feat: Desenvolvendo Pesquisar Contato

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ $(function () {
 	$("#botao-adicionar").on("click", function () {
 		if ($("#entrada").val() !== "") {
 			id++;
-			$("#lista").append("<li id='" + id + "'>" + $("#entrada").val() + "<button onclick=\"excluir(" + id + ")\">  X </button>" + "</li>");
+			var li = $("<li style='display: none' id='" + id + "'>" +  $("#entrada").val() + "<button onclick=\"excluir(" + id + ")\">  X </button>" + "</li>");
+
+			$("#lista").append(li);
+			li.fadeIn("fast");
+		} else {
+			alert("Não é possivel adicionar um contato vazio");
 		}
 		$("#entrada").val("");
 	});
@@ -14,6 +19,28 @@ $("#lista").on("click", "li", function () {
 });
 
 function excluir(id) {
-	$("li#" + id).remove();
+	$("li#" + id).fadeOut("slow", function(){
+		$(this).remove();
+	});
 }
+
+$("#sumir").on("click", function () {
+	$("#conteudo2").slideToggle("slow", function () {
+
+	});
+});
+
+$("#botao-pesquisar").on("click", function () {
+	var valor = $("#pesquisa").val().toLowerCase();
+	$("li")
+		.filter(function () {
+		var texto = $(this).text().toLowerCase();
+
+        if (texto.indexOf(valor) > -1) {
+            $(this).show("slow");
+        } else {
+            $(this).hide("slow");
+        }
+		});
+});
 
